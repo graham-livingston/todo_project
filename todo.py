cdabb957ec65eafbb27fece0819e9b37009564de
graham-livingston <glivingston@uchicago.edu>
2022-12-08 23:51:34 -0600

    updated todo.py

--- todo.py
+++ todo.py
@@ -1,6 +1,9 @@
 import os
 import pickle
+import uuid
 from datetime import datetime
+import argparse
+
 
 
 class Tasks:
@@ -16,7 +19,9 @@ class Tasks:
         """Read pickled tasks file into a list"""
         # List of Task objects
         self.tasks = self._load_tasks()
-        # your code here    
+
+    def __str__(self):
+        return (f'{self.tasks}')
         
     # def pickle_tasks(self):
     #     """Picle your task list to a file"""
@@ -36,8 +41,10 @@ class Tasks:
     # def query(self):
     #     pass
 
-    # def add(self):
-    #     pass
+    def add(self):
+        
+        pass
+    
     def _load_tasks(self):
         if os.path.isfile('.todo.pickle'):
         # look for the pickle file. and read it in if its found
@@ -51,8 +58,10 @@ class Tasks:
                     # make initial task of making todo list
                     
                     # add completed time
-                    
-                    pickle.dump("test object", f)
+                    task_list = []
+                    initialTask = Task('To do List', 1, '12.8.2022 10:40:00')
+                    task_list.append(initialTask)
+                    pickle.dump(task_list, f)
                 with open('.todo.pickle', 'rb') as f:
                     return (pickle.load(f))
                 # return (self._load_tasks())
@@ -80,12 +89,13 @@ class Task:
         self.name = name
         self.priority = priority
         self.due_date = self._parseDueDate(due_date) 
-        self.unique_id = "" # Generate a unique id
+        self.unique_id = uuid.uuid4() # Generate a unique id uuid.uuid4()
         self.created = datetime.now() # .timestamp() # self._time_created()
         self.complted = completed
         
     def __str__(self):
-        pass
+        return f'Name: {self.name}\nPriority: {self.priority}\nDue Date = {self.due_date}\nID: {self.unique_id}\nTime Created: {self.created}\nCompleted: {self.complted}'
+
             
     def _parseDueDate(self, due_date):
         if due_date == None:
@@ -94,11 +104,6 @@ class Task:
             # convert to datetime from user input. day.month.Year Hour:Min:Second
             date_time = datetime.strptime(due_date, '%d.%m.%Y %H:%M:%S')
             return date_time
-
-# def main():
-#     pass
-
-
 class DirectoryPermissionsError(Exception):
     """Exception raised for error when current directory cannont be written or read from"""
 
@@ -108,8 +113,44 @@ class DirectoryPermissionsError(Exception):
     def __str__(self):
         if self.message:
             return 'DirectoryPermissionsError, {0} '.format(self.message)
+def main():
+    """main _summary_
+    """
+    parser = argparse.ArgumentParser(description='update your ToDo list')
+    parser.add_argument('--add', type=str, required=False, help='a task string to add to your list')
+    parser.add_argument('--priority', type=int, required=False, default=1, help='priority of task; default value is 1')
+    
+    
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    # Create instance of Tasks
+    task_list = Tasks()
+    
+    # Read out arguments
+    if args.add:
+        print(f"we need to add {args.add} to the todo list with a priority of {args.priority}")
+        task_list.add(args.add, args.priority)
+    elif args.report:
+        print("print out the report")
+    
+
+#     pass
+
+
+
 
 # if __name__ == "__main__":
 #     main()
 
-x = Task()
+x = Tasks()
+print(x)
+with open('.todo.pickle', 'rb') as f:
+    testList = pickle.load(f)
+    newList = []
+    for testObj in testList:
+        newList.append(testObj)
+    newObj = Task('To do List 2', 1, '12.8.2022 11:40:00')
+    newList.append(newObj)
+    for obj in newList:
+        print(obj)

ff0bd94f2e1f7c1e7f4417c440f74a951daf0e1d
graham-livingston <glivingston@uchicago.edu>
2022-12-09 09:36:41 -0600

    draft list function

--- todo.py
+++ todo.py
@@ -18,19 +18,21 @@ class Tasks:
     def __init__(self):
         """Read pickled tasks file into a list"""
         # List of Task objects
+        tasks = []
         self.tasks = self._load_tasks()
 
     def __str__(self):
-        return (f'{self.tasks}')
+        for testObj in self.tasks:
+            return (f'{testObj}')
         
-    # def pickle_tasks(self):
-    #     """Picle your task list to a file"""
-    #     # your code here
-    #     pass  
+    def pickle_tasks(self):
+        """Picle your task list to a file"""
+        with open(".todo.pickle", 'wb') as f:
+            pickle.dump(self.tasks, f)  
     
-    # # Complete the rest of the methods, change the method definitions as needed
-    # def list(self):
-    #     pass
+    def list(self):
+        for objs in self.tasks:
+            print(objs)
 
     # def report(self):
     #     pass
@@ -41,15 +43,31 @@ class Tasks:
     # def query(self):
     #     pass
 
-    def add(self):
+    def add(self, name, priority, due_date=None):
+        """add _summary_
+
+        Arguments:
+            name -- _description_
+            priority -- _description_
+            due_date -- _description_
+        """
         
-        pass
+        
+        newTask = Task(name, priority, due_date)
+        # print(f'new_task: {newTask}')
+        return self.tasks.append(newTask)
+        # print(self.tasks)
+
     
     def _load_tasks(self):
         if os.path.isfile('.todo.pickle'):
         # look for the pickle file. and read it in if its found
             with open('.todo.pickle', 'rb') as f:
-                return (pickle.load(f))
+                # task_list = []
+                tasks = pickle.load(f)
+                # for task in tasks:
+                #     task_list.append(task)
+                return tasks
         # if it's not found then go ahead and make one
         else:
             try:
@@ -57,13 +75,16 @@ class Tasks:
                 with open(".todo.pickle", 'wb') as f:
                     # make initial task of making todo list
                     
-                    # add completed time
                     task_list = []
                     initialTask = Task('To do List', 1, '12.8.2022 10:40:00')
                     task_list.append(initialTask)
                     pickle.dump(task_list, f)
-                with open('.todo.pickle', 'rb') as f:
-                    return (pickle.load(f))
+                with open('.todo.pickle', 'rb') as f:  
+                    task_list = []
+                    tasks = pickle.load(f)
+                    for task in tasks:
+                        task_list.append(task)
+                    return tasks
                 # return (self._load_tasks())
             except:
                 raise DirectoryPermissionsError("Please provide read and write access to current working directory")
@@ -113,14 +134,20 @@ class DirectoryPermissionsError(Exception):
     def __str__(self):
         if self.message:
             return 'DirectoryPermissionsError, {0} '.format(self.message)
+
+
+
 def main():
     """main _summary_
     """
     parser = argparse.ArgumentParser(description='update your ToDo list')
     parser.add_argument('--add', type=str, required=False, help='a task string to add to your list')
     parser.add_argument('--priority', type=int, required=False, default=1, help='priority of task; default value is 1')
-    
-    
+    parser.add_argument('--due', type=str, required=False, help='due date in dd.mm.yyyy HH:MM:SS format')
+    parser.add_argument('--query', type=str, required=False, nargs="+", help='query by adding search terms')
+    parser.add_argument('--list', action='store_true', required=False, help="list all tasks that have not been completed")
+    parser.add_argument('--report', action='store_true', required=False, help="print formated report of all tasks")
+        
     # Parse the arguments
     args = parser.parse_args()
     
@@ -131,26 +158,29 @@ def main():
     if args.add:
         print(f"we need to add {args.add} to the todo list with a priority of {args.priority}")
         task_list.add(args.add, args.priority)
-    elif args.report:
-        print("print out the report")
+        print(task_list)
+    elif args.list:
+        task_list.list() 
     
-
-#     pass
-
-
+    task_list.pickle_tasks()
+    exit()
 
 
-# if __name__ == "__main__":
-#     main()
 
-x = Tasks()
-print(x)
-with open('.todo.pickle', 'rb') as f:
-    testList = pickle.load(f)
-    newList = []
-    for testObj in testList:
-        newList.append(testObj)
-    newObj = Task('To do List 2', 1, '12.8.2022 11:40:00')
-    newList.append(newObj)
-    for obj in newList:
-        print(obj)
+
+
+
+if __name__ == "__main__":
+    main()
+
+# x = Tasks()
+# print(x)
+# with open('.todo.pickle', 'rb') as f:
+#     testList = pickle.load(f)
+#     newList = []
+#     for testObj in testList:
+#         newList.append(testObj)
+#     newObj = Task('To do List 2', 1, '12.8.2022 11:40:00')
+#     newList.append(newObj)
+#     for obj in newList:
+#         print(obj)
